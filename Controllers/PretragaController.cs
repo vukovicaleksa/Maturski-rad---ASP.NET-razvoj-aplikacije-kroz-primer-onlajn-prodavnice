a28e6a7f1f2623a5d303b2558bd375f3938cc332
Алекса Вуковић <2021.09.03@online.devetagimnazija.edu.rs>
2025-01-16 19:33:39 +0100

    16.1.25. - Ne radi menjanje naslova za pretragu

---
+++ Controllers/PretragaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Maturski_rad___ASP.NET_razvoj_aplikacije_kroz_primer_onlajn_prodavnice.Controllers
+{
+    public class PretragaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Pretrazi(string tekstPretrage)
+        {
+            TempData["pretraga"] = tekstPretrage;
+            return View("~/Views/_RezultatiPretrage.cshtml");
+        }
+    }
+}
