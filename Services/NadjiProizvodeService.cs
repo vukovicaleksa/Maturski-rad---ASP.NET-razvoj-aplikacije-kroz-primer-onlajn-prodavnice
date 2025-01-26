c444eef8733e1d61db8ec00c99272a74207231eb
Алекса Вуковић <2021.09.03@online.devetagimnazija.edu.rs>
2025-01-26 09:51:00 +0100

    26.1. Krenuo da dodajem bazu pomocu OleDb-a, ali ne znam da li moze tako jer se ne podrazumeva da bi svi korisnici imali windows.

---
+++ Services/NadjiProizvodeService.cs
@@ -0,0 +1,31 @@
+﻿using Maturski_rad___ASP.NET_razvoj_aplikacije_kroz_primer_onlajn_prodavnice.Models;
+using System.Text.Json;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Maturski_rad___ASP.NET_razvoj_aplikacije_kroz_primer_onlajn_prodavnice.Services
+{
+    public class NadjiProizvodeService
+    {
+        public NadjiProizvodeService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+            oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+                WebHostEnvironment.WebRootPath + "\\data\\Baza.accdb;Persist Security Info=True");
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public OleDbConnection oleDbConnection { get; }
+
+        private string LokacijaBaze
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "Baza.accdb"); }
+        }
+
+        public IEnumerable<Proizvod> ListaProizvoda()
+        {
+            return null;
+        }
+    }
+}
